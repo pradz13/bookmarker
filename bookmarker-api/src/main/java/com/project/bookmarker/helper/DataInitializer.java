b532da0731da64b4a4c268d554acf9c34655b9df
Pradipta <pradipta.nag2@gmail.com>
2023-08-27 22:13:01 +0530

    Adding some default bookmarks

---
+++ bookmarker-api/src/main/java/com/project/bookmarker/helper/DataInitializer.java
@@ -0,0 +1,22 @@
+package com.project.bookmarker.helper;
+
+import com.project.bookmarker.domain.Bookmark;
+import com.project.bookmarker.repository.BookmarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookmarkRepository bookmarkRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        bookmarkRepository.save(new Bookmark(null, "Google", "https://www.google.com", Instant.now()));
+        bookmarkRepository.save(new Bookmark(null, "Instagram", "https://www.instagram.com", Instant.now()));
+    }
+}

e616027db63f71bf826ff488261f26c2f65dffcd
Pradipta <pradipta.nag2@gmail.com>
2023-09-17 21:42:23 +0530

    Adding tests

--- bookmarker-api/src/test/java/com/project/bookmarker/api/BookmarkControllerTest.java
+++ bookmarker-api/src/test/java/com/project/bookmarker/api/BookmarkControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -21,6 +22,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///demo"
+})
 class BookmarkControllerTest {
 
     @Autowired
